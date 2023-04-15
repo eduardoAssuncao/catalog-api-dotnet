699bdb06fc37d8df1f686f787295f7658d0f5a4b
Eduardo Sousa <eduardoasssousa@gmail.com>
2023-04-15 10:46:04 +0100

    Implementação do get, put e delete

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
 using Catalog.Repositories;
+using Catalog.Settings;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>();// Add services to the container. Singleton é usado para criar uma instância única do repositório
+
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));//registra o serializador do Guid para ser salvo como string no banco de dados
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));//registra o serializador do DateTimeOffset para ser salvo como string no banco de dados
+
+builder.Services.AddSingleton<IMongoClient>(ServiceProvider => //Singleton é usado para criar uma instância única do MongoClient
+{
+    var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();//pega as configurações do appsettings.json
+    return new MongoClient(settings.ConnectionString);//retorna uma instância do MongoClient
+});
+
+builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();// Add services to the container. Singleton é usado para criar uma instância única do repositório
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- Repositories/MongoDbItemsRepository.cs
+++ Repositories/MongoDbItemsRepository.cs
@@ -1,43 +1,48 @@
 using Catalog.Entities;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Catalog.Repositories
 {
     public class MongoDbItemsRepository : IItemsRepository
     {
-        private const string databaseName = "catalog";
-        private const string collectionName = "items";
-        private readonly IMongoCollection<Item> itemsCollection;
+        private const string databaseName = "catalog";//nome do banco de dados
+        private const string collectionName = "items";//nome da coleção
+        private readonly IMongoCollection<Item> itemsCollection;//cria uma coleção de itens
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;//cria um filtro para a busca
 
-        public MongoDbItemsRepository(IMongoClient mongoClient)
+        public MongoDbItemsRepository(IMongoClient mongoClient)//injeção de dependência do MongoClient
         {
-            IMongoDatabase database = mongoClient.GetDatabase(databaseName);
-            itemsCollection = database.GetCollection<Item>(collectionName);
+            IMongoDatabase database = mongoClient.GetDatabase(databaseName);//pega o banco de dados
+            itemsCollection = database.GetCollection<Item>(collectionName);//pega a coleção de itens
         }
 
         public void CreateItem(Item item)
         {
-            throw new NotImplementedException();
+            itemsCollection.InsertOne(item);//insere um item na coleção
         }
 
         public void DeleteItem(Guid id)
         {
-            throw new NotImplementedException();
+            var filter = filterBuilder.Eq(item => item.Id, id);//cria um filtro para a busca
+            itemsCollection.DeleteOne(filter);//deleta um item da coleção
         }
 
         public Item GetItem(Guid id)
         {
-            throw new NotImplementedException();
+            var filter = filterBuilder.Eq(item => item.Id, id);//cria um filtro para a busca
+            return itemsCollection.Find(filter).SingleOrDefault();//retorna o item encontrado
         }
 
         public IEnumerable<Item> GetItems()
         {
-            throw new NotImplementedException();
+            return itemsCollection.Find(new BsonDocument()).ToList();//retorna todos os itens da coleção
         }
 
         public void UpdateItem(Item item)
         {
-            throw new NotImplementedException();
+            var filter = filterBuilder.Eq(existingItem => existingItem.Id, item.Id);//cria um filtro para a busca
+            itemsCollection.ReplaceOne(filter, item);//atualiza o item
         }
     }
 }
